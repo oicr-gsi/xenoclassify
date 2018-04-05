0f38a18b1be40b4e780d20041de1791ba2ee250a
heathkd <heather.dsouza@hotmail.com>
2018-04-05 09:02:27 -0400

    convert bam reads to fastq immediately and output paired-end reads in separate files

--- pipeline/classifyByAS.py
+++ pipeline/classifyByAS.py
@@ -8,15 +8,14 @@ def parse_input ():
 	parser.add_argument('-f', '--fastq', help='Output FASTQ files', action='store_true')
 	parser.add_argument('-p', '--prefix', help='Prefix for BAM and FASTQ file names')
 	args = parser.parse_args()
-	print(args)
 	return parser, args 
 
 # error handling
 def is_valid_file(parser, arg):
     if not os.path.isfile(arg):
         parser.error("The file %s does not exist" % arg)
-    # if not re.search(r'(?<=[.])\w+',arg).group(0) == 'bam':
-    #  	parser.error("The file %s is not a bam file" % arg)
+    if not re.search(r'(?<=[.])\w+',arg).group(0) == 'bam':
+     	parser.error("The file %s is not a bam file" % arg)
     else:
     	return arg
 
@@ -43,27 +42,46 @@ def initialize_user_input(args):
 	prefix = args.prefix
 	return mouse_bam, human_bam, output_dir, is_bam, is_fastq, prefix
 
-def create_fastq_output(bam):
+def create_fastq_output(output_dir, prefix):
 	# create output files
-	# FASTQs
-	graft = AlignmentFile("graft.bam", "wb", template = bam)
-	host = AlignmentFile("host.bam", "wb", template = bam)
-	both = AlignmentFile("both.bam", "wb", template = bam)
-	ambiguous = AlignmentFile("ambiguous.bam", "wb", template = bam)
-	neither = AlignmentFile("neither.bam", "wb", template = bam)
+	if prefix is None:
+		prefix = ""
+	else:
+		prefix = prefix + "_"
 
-	fastq_output = {
-		"graft": graft,
-		"host": host,
-		"both": both,
-		"ambiguous": ambiguous,
-		"neither": neither,
+	graft_1 = open("{:s}/{:s}graft_1.fastq".format(output_dir, prefix),"w")
+	host_1 = open("{:s}/{:s}host_1.fastq".format(output_dir, prefix),"w")
+	both_1 = open("{:s}/{:s}both_1.fastq".format(output_dir, prefix),"w")
+	ambiguous_1 = open("{:s}/{:s}ambiguous_1.fastq".format(output_dir, prefix),"w")
+	neither_1 = open("{:s}/{:s}neither_1.fastq".format(output_dir, prefix),"w")
+
+	graft_2 = open("{:s}/{:s}graft_2.fastq".format(output_dir, prefix),"w")
+	host_2 = open("{:s}/{:s}host_2.fastq".format(output_dir, prefix),"w")
+	both_2 = open("{:s}/{:s}both_2.fastq".format(output_dir, prefix),"w")
+	ambiguous_2 = open("{:s}/{:s}ambiguous_2.fastq".format(output_dir, prefix),"w")
+	neither_2 = open("{:s}/{:s}neither_2.fastq".format(output_dir, prefix),"w")
+
+	fastq_output_1 = {
+		"graft": graft_1,
+		"host": host_1,
+		"both": both_1,
+		"ambiguous": ambiguous_1,
+		"neither": neither_1,
 	}
-	return fastq_output
 
-def create_bam_output(bam):
+	fastq_output_2 = {
+		"graft": graft_2,
+		"host": host_2,
+		"both": both_2,
+		"ambiguous": ambiguous_2,
+		"neither": neither_2,
+	}
+
+	return fastq_output_1, fastq_output_2
+
+def create_bam_output(bam, output_dir):
 	# BAMs
-	out_bam = AlignmentFile("output.bam", "wb", template = bam)
+	out_bam = AlignmentFile("{:s}/output.bam".format(output_dir), "wb", template = bam)
 	return out_bam
 	
 def initialize_counters():
@@ -135,36 +153,7 @@ def add_tag(read, category, output_bam):
 		output_bam.write(read)
 
 def add_to_fastq(read, fastq):
-	fastq.write(read)
-
-# def activate_functions(is_bam, is_fastq):
-
-# 	def add_tag(read, category, output_bam):
-# 		read.set_tag('CL',category)
-# 		output_bam.write(read)
-
-# 	def add_to_fastq(read, fastq):
-# 		fastq.write(read)
-
-# 	if is_bam and is_fastq:
-# 		def call_functions(read, category, output_bam, fastq):
-# 			add_tag(read, category, output_bam)
-# 			add_to_fastq(read, fastq)
-# 	elif is_bam:
-# 		def call_functions(read, category, output_bam):
-# 			add_tag(read, category, output_bam)
-# 	elif is_fastq:
-# 		def call_functions(read, fastq):
-# 			add_to_fastq(read, fastq)
-# 	else:
-# 		def call_functions():
-# 			print("")
-
-def convert_to_fastq(fastq, prefix):
-	if prefix is None:
-		Bedtool.bam_to_fastq(i=file, fq="{:s}_1.fastq".format(fastq), fq2="{:s}_2.fastq".format(fastq))
-	else:
-		Bedtool.bam_to_fastq(i=file, fq="{:s}_{:s}_1.fastq".format(prefix, fastq), fq2="{:s}_{:s}_2.fastq".format(prefix, fastq))
+	fastq.write("@{:s}\n{:s}\n+\n{:s}\n".format(read.query_name, read.query_sequence, "".join([chr(x + 33) for x in read.query_qualities])))
 
 # calculate class percentages
 def calculate_percentage(count, total_count):
@@ -185,8 +174,7 @@ if __name__ == '__main__':
 	import os
 	import tempfile
 	from pysam import AlignmentFile 
-	from pybedtools import Bedtool
-	
+
 	try:
 		# Python 3
 		from itertools import zip_longest
@@ -197,17 +185,16 @@ if __name__ == '__main__':
 	parser, args = parse_input()
 	is_prefix_dependent(parser, args)
 	mouse_bam, human_bam, output_dir, is_bam, is_fastq, prefix = initialize_user_input(args)
-	# activate_functions(is_bam, is_fastq)
 	if is_fastq:
-		fastq_output = create_fastq_output(human_bam)
+		fastq_output_1, fastq_output_2 = create_fastq_output(output_dir, prefix)
 	if is_bam:
-		output_bam = create_bam_output(human_bam)
+		output_bam = create_bam_output(human_bam, output_dir)
 	counters = initialize_counters()
 
-	mouse_primary, mouse_secondary = filter_bam(mouse_bam, 'mouse') # change variable names to graft_bam and host_bam?
-	human_primary, human_secondary = filter_bam(human_bam, 'human')
-	# human_primary = AlignmentFile('/.mounts/labs/gsiprojects/gsi/Xenograft-Classifier/data/seqware_prov_report/projects/AMLXP/bwa/hg19/classifyByAS_results/new_rules/6816887/hg19_primary.bam', 'rb')
-	# mouse_primary = AlignmentFile('/.mounts/labs/gsiprojects/gsi/Xenograft-Classifier/data/seqware_prov_report/projects/AMLXP/bwa/hg19/classifyByAS_results/new_rules/6816887/mm10_primary.bam', 'rb')	
+	# mouse_primary, mouse_secondary = filter_bam(mouse_bam, 'mouse') # change variable names to graft_bam and host_bam?
+	# human_primary, human_secondary = filter_bam(human_bam, 'human')
+	human_primary = AlignmentFile('/.mounts/labs/gsiprojects/gsi/Xenograft-Classifier/data/seqware_prov_report/projects/AMLXP/bwa/hg19/classifyByAS_results/new_rules/6816887/hg19_primary.bam', 'rb')
+	mouse_primary = AlignmentFile('/.mounts/labs/gsiprojects/gsi/Xenograft-Classifier/data/seqware_prov_report/projects/AMLXP/bwa/hg19/classifyByAS_results/new_rules/6816887/mm10_primary.bam', 'rb')	
 	
 	# fetch() only reads in one read at a time. read_count ensures that the following methods run only if two reads have been read in
 	read_count = 0 	
@@ -229,19 +216,14 @@ if __name__ == '__main__':
 			check_read_names(mr1_name, mr2_name, hr1_name, hr2_name)
 			classification = classify(mr1_AS, mr2_AS, hr1_AS, hr2_AS)
 			increment_counters(classification, counters)
-			for read in human_reads:
-				if is_bam:
+			if is_bam:
+				for read in human_reads:
 					add_tag(read, classification, output_bam)
-				if is_fastq:
-					add_to_fastq(read, fastq_output[classification])
-				# call_functions(read, classification, output_bam, read, fastq_output[classification])
+			if is_fastq:
+				add_to_fastq(human_reads[0], fastq_output_1[classification])
+				add_to_fastq(human_reads[1], fastq_output_2[classification])
 
 	# output
 	for key in counters:
 		percentages.append(calculate_percentage(counters[key],counters["total"]))
 	display_output(percentages)
-
-	#fastq
-	if is_fastq is True:
-		for file in fastq_output:
-			convert_to_fastq(file, prefix)
